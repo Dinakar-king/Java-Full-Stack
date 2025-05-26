b269f590e4ff9741a9ebf0d052aeafa4ac261abd
Tupakula Dinakar <thupakuladinkar3@gmail.com>
2025-05-26 15:24:05 +0530

    Add files via upload

---
+++ management/src/main/java/com/example/management/model/SentimentResponse.java
@@ -0,0 +1,10 @@
+package com.example.management.model;
+
+import lombok.Data;
+
+@Data
+public class SentimentResponse {
+        private String text;
+        private String label;
+        private float score;
+}
